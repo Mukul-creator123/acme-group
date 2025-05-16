78f39044a193cdf86b00abd27a2130dd7efe9e43
Mukul113-creator <smukul934@gmail.com>
2025-05-16 16:02:35 +0530

    page design

--- src/app/FounderCard/FounderCard.js
+++ src/app/FounderCard/FounderCard.js
@@ -18,13 +18,13 @@ export default function FounderCard() {
                 />
             </div>
 
-            <div className="z-10 flex-1 cardWidth pl-30">
+            <div className="z-10 flex-1 cardWidth pl-25">
                 <p className="text-sm text-gray-500 text-center lg:text-start">
                     Meet Founder of <span className="text-orange-500 font-semibold">ACME</span> Group
                 </p>
                 <h2 className="text-4xl font-bold text-gray-800 mt-1 text-center lg:text-start">Ramon Talwwar</h2>
-                <p className="text-2xl font-medium text-gray-700 mt-2 text-center lg:text-start">
-                    Not all dreamers are winners, but all winners are dreamers.
+                <p className="text-[22px] font-medium text-gray-700 mt-2 text-center lg:text-start">
+                    Not all dreamers are winners , but all winners are dreamers.
                 </p>
                 <p className="text-sm text-gray-600 mt-2 text-center lg:text-start">
                     Ramon Talwwar, a results-focused strategic professional with over two decades of
@@ -32,18 +32,22 @@ export default function FounderCard() {
                 </p>
             </div>
 
-            <div className="z-10 w-full md:w-64 mt-6 lg:pb-0 pb-6 md:mt-0 border-l border-gray-300 pl-6 cardWidth">
+            <div className="z-10 w-full font-bold md:w-64 mt-6 lg:pb-0 pb-6 md:mt-0 border-l border-gray-300 pl-8 cardWidth">
                 <h3 className="text-md font-semibold text-gray-800 mb-3 text-center lg:text-start">Recent Posts</h3>
                 <ul className="text-sm text-gray-600 space-y-2">
-                    <li className="text-center lg:text-start">
+                    <li className="text-center mb-6 lg:text-start ">
                         <span className="text-xs text-gray-400">July 9, 2024</span><br />
                         Derivatives Update for Tuesday, 9th July 2024
                     </li>
-                    <li>
+                    <li className="mb-6">
                         <span className="text-xs text-gray-400">July 9, 2024</span><br />
                         Market Outlook, 9th July 2024
                     </li>
-                    <li>
+                    <li  className="mb-6">
+                        <span className="text-xs text-gray-400">July 9, 2024</span><br />
+                        Derivatives Update for Tuesday, 9th July 2024
+                    </li>
+                    <li  className="mb-6">
                         <span className="text-xs text-gray-400">July 9, 2024</span><br />
                         Derivatives Update for Tuesday, 9th July 2024
                     </li>
